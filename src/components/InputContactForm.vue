3d3ff304834f7d803571cad8401e1eebc058bb8e
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-21 09:42:39 +0700

    docs: add api docs and update step one on work instructions section

--- src/components/InputContactForm.vue
+++ src/components/InputContactForm.vue
@@ -60,7 +60,7 @@ export default {
   },
   methods: {
     async onSubmit() {
-      // Uncomment baris kode di bawah untuk men-dispatch fungsi mengirim data kontak baru yang sudah dibuat sebelumnya di store/index.js lalu panggil fungsi untuk mengambil semua data kontak dari api dan mereset value yang ada di setiap input field
+      // TODO: Uncomment baris kode di bawah untuk men-dispatch fungsi mengirim data kontak baru yang sudah dibuat sebelumnya di store/index.js lalu panggil fungsi untuk mengambil semua data kontak dari api dan mereset value yang ada di setiap input field
       // await this.$store.dispatch("addNewContact", {
       //   full_name: this.input.full_name,
       //   phone_number: this.input.phone_number,
--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,7 @@ export default new Vuex.Store({
         return error;
       });
     },
-    // Uncomment baris kode dibawah untuk membuat sebuah fungsi yang akan mengirimkan data kontak baru ke API
+    // TODO: Uncomment baris kode dibawah untuk membuat sebuah fungsi yang akan mengirimkan data kontak baru ke API
     // async addNewContact(context, payload) {
     //   return await axios
     //     .post(`${BASE_URL}/contacts`, payload)
